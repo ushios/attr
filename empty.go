8b0b51daf2ab41cd9c5be21af3aa43e13ceba5b8
Ushio Shugo <ushio.s@gmail.com>
2016-08-13 19:15:43 +0900

    Add SliceIsEmpty (#1)
    * Add slice empty functions
    
    * Add SliceIsEmpty function.
    
    * Add SliceIsEmpty nil test
    
    * Add InvalidIsEmpty
    make panic when the kind is not supported.
    
    * add tests.

--- empty.go
+++ empty.go
@@ -1,21 +1,32 @@
 package attr
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 // IsEmpty check the value is empty or not
 // empty: "", 0, nil,
 func IsEmpty(v interface{}) bool {
 	r := reflect.ValueOf(v)
+
 	switch r.Kind() {
+	case reflect.Int:
+		i := v.(int)
+		return IntIsEmpty(int64(i))
 	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		return IntIsEmpty(v.(int64))
 	case reflect.Float32, reflect.Float64:
 		return FloatIsEmpty(v.(float64))
 	case reflect.String:
 		return StringIsEmpty(v.(string))
+	case reflect.Slice:
+		return SliceIsEmpty(v)
+	case reflect.Invalid:
+		return InvalidIsEmpty(v)
 	}
 
-	return true
+	panic(fmt.Sprintf("we have no case for (%v: %v) ", r.Kind(), v))
 }
 
 // IntIsEmpty check the int is empty or not
@@ -40,4 +51,32 @@ func FloatIsEmpty(v float64) bool {
 		return false
 	}
 	return true
+}
+
+// SliceIsEmpty check the slice is empty or not
+func SliceIsEmpty(v interface{}) bool {
+	if v == nil {
+		return true
+	}
+
+	r := reflect.ValueOf(v)
+	switch r.Kind() {
+	case reflect.Slice:
+		if r.Len() == 0 {
+			return true
+		}
+
+		return false
+	}
+
+	panic(fmt.Sprintf("%v is not slice", v))
+}
+
+// InvalidIsEmpty check the invalid value is empty or not
+func InvalidIsEmpty(v interface{}) bool {
+	if v == nil {
+		return true
+	}
+
+	return false
 }
--- empty_test.go
+++ empty_test.go
@@ -2,6 +2,30 @@ package attr
 
 import "testing"
 
+func TestIsEmpty(t *testing.T) {
+	test := func(v interface{}, e bool) {
+		res := IsEmpty(v)
+
+		if res != e {
+			t.Errorf("(%v) expected empty (%t) but (%t)", v, e, res)
+		}
+	}
+
+	test(0, true)
+	test(1, false)
+	test(-1, false)
+	test("", true)
+	test("hoge", false)
+	test(123456789123456789, false)
+	test(0.0, true)
+	test(0.1, false)
+	test(0.0000001, false)
+	test([]string{}, true)
+	test([]string{"a"}, false)
+	test([]int{1}, false)
+	test(nil, true)
+}
+
 func TestIntIsEmpty(t *testing.T) {
 	test := func(v int64, e bool) {
 		res := IntIsEmpty(v)
@@ -41,4 +65,35 @@ func TestFloatIsEmpty(t *testing.T) {
 	test(0.0, true)
 	test(0.1, false)
 	test(0.0000001, false)
+}
+
+func TestSliceIsEmpty(t *testing.T) {
+	test := func(v interface{}, e bool) {
+		res := SliceIsEmpty(v)
+
+		if res != e {
+			t.Errorf("(%v) expected empty (%t) but (%t)", v, e, res)
+		}
+	}
+
+	test([]string{}, true)
+	test([]string{"a"}, false)
+	test([]int{1}, false)
+	test(nil, true)
+
+	var s []int
+	test(s, true)
+}
+
+func TestInvalidIsEmpty(t *testing.T) {
+	test := func(v interface{}, e bool) {
+		res := InvalidIsEmpty(v)
+
+		if res != e {
+			t.Errorf("(%v) expected empty (%t) but (%t)", v, e, res)
+		}
+	}
+
+	test(nil, true)
+	test(1, false)
 }
